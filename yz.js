6ead10bc8bf5404edc5da996120807caa9ef88c6
fusion6108 <9919012@qq.com>
2024-08-12 23:31:47 +0800

    Update yz.js

--- yz.js
+++ yz.js
@@ -13,31 +13,33 @@ hostname = oneplay-api.instadrum.com， oneplay-api.xiaoyezi.com
 var body = $response.body;
 var url = $request.url;
 
-if (url.indexOf("/drum/score/") != -1){
-    body = body.replace(/\"package_status\":0/g, "\"package_status\":1"); 
-    body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
-    body = body.replace(/"account_status":\s*0,/g, "\"account_status\": 1,");//是否拥有
-    body = body.replace(/"product_type":\s*"1"/g, "\"product_type\": \"0\"");
-}
-if (url.indexOf("/drum/course/user_behaviour") != -1){
-    body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
-    body = body.replace(/"process_rate":\d+/g, "\"process_rate\":100");//进度
+try {
+    if (url.includes("/drum/score/")){
+        body = body.replace(/\"package_status\":0/g, "\"package_status\":1"); 
+        body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
+        body = body.replace(/"account_status":\s*0,/g, "\"account_status\": 1,");//是否拥有
+        body = body.replace(/"product_type":\s*"1"/g, "\"product_type\": \"0\"");
+    }
+    if (url.includes("/drum/course/user_behaviour")){
+        body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
+        body = body.replace(/"process_rate":\d+/g, "\"process_rate\":100");//进度
 
-} 
+    } else if (url.includes("/drum/account")){
+        let obj = JSON.parse(body);
+        obj.data.subscribe = {
+            "status": 1,
+            "start_time": "19999999999",
+            "end_time": "19999999999",
+            "auto_renew": "1"
+        };
+        body = JSON.stringify(obj);
+    } else if (url.includes("/drum/lesson/add_process")){
+        body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
+        body = body.replace(/"process_rate":\d+/g, "\"process_rate\":100");//进度
 
-else if (url.indexOf("/drum/account") != -1){
-    let obj = JSON.parse(body);
-    obj.data.subscribe = {
-        "status": 1,
-        "start_time": "19999999999",
-        "end_time": "19999999999",
-        "auto_renew": "1"
-    };
-    body = JSON.stringify(obj);
-} else if (url.indexOf("/drum/lesson/add_process") != -1){
-    body = body.replace(/"unlock_status":0/g, "\"unlock_status\":1");//是否解锁
-    body = body.replace(/"process_rate":\d+/g, "\"process_rate\":100");//进度
-
+    }
+} catch (e) {
+    console.error("Error processing response:", e);
 }
 
 $done({body});
